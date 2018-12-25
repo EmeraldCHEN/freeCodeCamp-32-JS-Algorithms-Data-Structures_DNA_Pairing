6f50b62d61b1d7407c583804e6e5ad2d476ec88b
EmeraldNZ <ahyuancy@gmail.com>
2018-12-25 14:18:48 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,6 @@
+
+/***************   Solution #1     *****************************************************************************************/
+
 function pairElement(str) {
     let arr = str.split('');
     var twoDArr = [];
@@ -14,3 +17,43 @@ function pairElement(str) {
     }
     return twoDArr;
   }
+pairElement("ATCGA"); // return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+pairElement("TTGA"); // return [["T","A"],["T","A"],["G","C"],["A","T"]]
+
+
+/***************   Solution #2     *****************************************************************************************/
+
+
+function pairElement(str) {
+    let paired = [];
+    // Function to check with strand to pair using switch statements
+    let search = function(char){ // Same as:  function search(char){ 
+        switch(char){
+            case 'A':
+                paired.push(['A','T']);
+                break;
+            case 'T':
+                paired.push(['T','A']);
+                break; 
+            case 'C':
+                paired.push(['C','G']);
+                break;
+            case 'G':
+                paired.push(['G','C']);
+                break;
+        }
+    };
+    // Loops through the input and pair
+    for(let i = 0; i < str.length; i++){
+        search(str[i]);
+    }
+    return paired;
+}
+
+
+
+
+
+
+// Retrieved from
+// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/dna-pairing/
